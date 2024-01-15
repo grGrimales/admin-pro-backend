4da3544d48a45181aae328d916c3fbdf6751ed84
Carlos Guzman <guzmandata@gmail.com>
2024-01-15 07:50:34 -0500

    version incial proyecto admin pro

---
+++ index.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const path = require('path');
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+
+// Crear el servidor de express
+const app = express();
+
+
+// Configurar CORS para permitir solicitudes de localhost:3000
+const corsOptions = {
+    origin: 'http://localhost:3000', // Define el origen permitido
+  };
+
+
+
+// Configurar CORS
+app.use(cors(corsOptions));
+
+
+
+
+// Lectura y parseo del body
+app.use( express.json() );
+
+// Base de datos
+dbConnection();
+
+// Directorio público
+app.use( express.static('public') );
+
+// Rutas
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/hospitales', require('./routes/hospitales') );
+app.use( '/api/medicos', require('./routes/medicos') );
+app.use( '/api/todo', require('./routes/busquedas') );
+app.use( '/api/login', require('./routes/auth') );
+app.use( '/api/upload', require('./routes/uploads') );
+
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
+
+
+app.listen( process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
+});
+
