c6305daa2fe6a7bd6268f8f80d5eed09eaf9834a
Carlos Guzman <guzmandata@gmail.com>
2024-01-15 20:23:58 -0500

    configurar cors

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const app = express();
 const corsOptions = {
     origin: 'http://localhost:3000',
     origin: 'https://admin-pro-front.vercel.app/',
+    origin: 'https://admin-pro-front.vercel.app',
 
 
   };
